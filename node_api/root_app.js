0ba77e3f67269004192e74a876a6ad47e84c4f82
Mathieu-Poirier <mathieuworkemail@gmail.com>
2023-09-27 09:41:37 -0400

    Created HTTP response from node API

--- node_api/root_app.js
+++ node_api/root_app.js
@@ -1,2 +1,14 @@
 const express = require('express');
 const app = express();
+const PORT = 4100;
+
+app.get('/getData', (req, res) => {
+    res.json({
+        statusCode:200,
+        "statusMessage":"Sucess"
+    })
+})
+
+app.listen(PORT, (req, res) =>{
+    console.log("Express API is running on port " + PORT);
+})
