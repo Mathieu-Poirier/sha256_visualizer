1634f0c2ec54a5fb7b1b566964032573ccfdb219
Mathieu-Poirier <mathieuworkemail@gmail.com>
2023-09-27 15:49:57 -0400

    Created title and text-area components

--- sha256_frontend/src/app/app.module.ts
+++ sha256_frontend/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from  '@angular/common/http';
+import { TitleComponent } from './title/title.component';
+import { TextAreaInputComponent } from './text-area-input/text-area-input.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TitleComponent,
+    TextAreaInputComponent
   ],
   imports: [
     BrowserModule,
---
+++ sha256_frontend/src/app/text-area-input/text-area-input.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-text-area-input',
+  templateUrl: './text-area-input.component.html',
+  styleUrls: ['./text-area-input.component.css']
+})
+export class TextAreaInputComponent {
+
+}
