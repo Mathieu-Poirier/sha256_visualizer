34c52e123b3e446284b788c19119e4ad47277caf
Mathieu-Poirier <mathieuworkemail@gmail.com>
2023-09-27 18:31:02 -0400

    console.log() value of textarea

--- sha256_frontend/src/app/text-area-input/text-area-input.component.ts
+++ sha256_frontend/src/app/text-area-input/text-area-input.component.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-text-area-input',
   templateUrl: './text-area-input.component.html',
-  styleUrls: ['./text-area-input.component.css']
+  styleUrls: ['./text-area-input.component.css'],
 })
 export class TextAreaInputComponent {
-
+  load(input_val: any){
+    console.log(input_val);
+  }
 }
